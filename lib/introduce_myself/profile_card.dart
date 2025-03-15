46651b60e05ea373b6d4c56f418cb406e16eb1e5
SandyKnluck <nimnaul_k@su.ac.th>
2025-03-15 09:45:08 +0700

    ready_for_exam

--- lib/introduce_myself/profile_card.dart
+++ lib/introduce_myself/profile_card.dart
@@ -27,7 +27,44 @@ class ProfileCard extends StatelessWidget {
           child: const Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-             
+             CircleAvatar(
+                radius: 40,
+                backgroundImage: NetworkImage(
+                    'https://static1.cbrimages.com/wordpress/wp-content/uploads/2022/12/anya-forger-heh.jpg'),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Kanhokluck Nimnaul',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                '650710520',
+                style: TextStyle(color: Colors.black),
+              ),
+              Text(
+                '	Computer Science Major',
+                style: TextStyle(color: Colors.grey),
+              ),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.email, size: 16, color: Colors.blue),
+                  SizedBox(width: 5),
+                  Text('nimnaul_k@silpakorn.edu'),
+                ],
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.phone, size: 16, color: Colors.green),
+                  SizedBox(width: 5),
+                  Text('0968815526'),
+                ],
+              ),
             ],
           ),
         )));
